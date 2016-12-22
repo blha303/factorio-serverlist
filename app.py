85e440a68db871255644d8a052f60b9b8a66a5da
blha303 <stevensmith.ome@gmail.com>
2016-12-22 20:02:30 +0800

    Now with working web interface

--- app.py
+++ app.py
@@ -1,31 +1,36 @@
 #!/usr/bin/env python3
 from flask import *
-from flask.ext.cache import Cache
 import requests
+import requests_cache
 
 from os import environ
 from json import load
 from sys import exit
 
 app = Flask(__name__)
-cache = Cache(app,config={"CACHE_TYPE": "simple"})
+requests_cache.install_cache(backend='sqlite', expire_after=180)
 try:
     with open(environ["HOME"] + "/factorio/player-data.json") as f:
         credentials = load(f)
 except:
-    print("No user credentials. Get the multiplayer dedicated server, put it at {}/factorio, add your user account to its settings and run it once".format(environ["HOME"]))
+    print("No user credentials. Get the multiplayer dedicated server, put it at {}/factorio, add your user account to its settings and run it once".format(environ["HOME"] if "HOME" in environ else "{user directory}"))
     exit(1)
 
-@cache.cached(timeout=10)
-@app.route("/server/<id>")
-def serverinfo(id):
+@app.route("/api/server/<id>")
+def infojson(id):
     return jsonify(requests.get("https://multiplayer.factorio.com/get-game-details/{}".format(id)).json())
 
-@cache.cached(timeout=60)
-@app.route("/server")
-@app.route("/server/")
+@app.route("/api/list")
 def serverlist():
-    return jsonify(requests.get("https://multiplayer.factorio.com/get-games?username={service-username}&token={service-token}".format(**credentials)).json())
+    data = requests.get("https://multiplayer.factorio.com/get-games?username={service-username}&token={service-token}".format(**credentials)).json()
+    for d in data:
+        d["has_password"] = True if d["has_password"] == "true" else False
+    return jsonify(data)
+
+@app.route("/info/<id>")
+def infohtml(id):
+    data = requests.get("https://multiplayer.factorio.com/get-game-details/{}".format(id)).json()
+    return render_template("info.html", d=data, title=data["name"])
 
 @app.route("/")
 def index():
