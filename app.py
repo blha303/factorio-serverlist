993e69df958914bccbb568f1b8a119b018dbb788
blha303 <stevensmith.ome@gmail.com>
2016-12-23 08:39:22 +0800

    Added sorting, game time

--- app.py
+++ app.py
@@ -16,6 +16,21 @@ except:
     print("No user credentials. Get the multiplayer dedicated server, put it at {}/factorio, add your user account to its settings and run it once".format(environ["HOME"] if "HOME" in environ else "{user directory}"))
     exit(1)
 
+@app.route("/api/getmod/<slug>")
+@app.route("/api/getmod/<slug>/<version>")
+def getmod(slug, version="latest"):
+    if slug == "base":
+        return redirect(request.referrer)
+    data = requests.get("https://mods.factorio.com/api/mods/{}".format(slug)).json()
+    if data.get("detail", None) == "Not found.":
+        return jsonify({"error":"Can't find that mod"})
+    if version == "latest":
+        return redirect("https://mods.factorio.com" + data["releases"][0]["download_url"])
+    else:
+        for file in data["releases"]:
+            if file["version"] == version:
+                return redirect("https://mods.factorio.com" + file["download_url"])
+
 @app.route("/api/server/<id>")
 def infojson(id):
     return jsonify(requests.get("https://multiplayer.factorio.com/get-game-details/{}".format(id)).json())
@@ -23,6 +38,9 @@ def infojson(id):
 @app.route("/api/list")
 def serverlist():
     data = requests.get("https://multiplayer.factorio.com/get-games?username={service-username}&token={service-token}".format(**credentials)).json()
+    if not data:
+        with requests_cache.disabled():
+            data = requests.get("https://multiplayer.factorio.com/get-games?username={service-username}&token={service-token}".format(**credentials)).json()
     for d in data:
         d["has_password"] = True if d["has_password"] == "true" else False
     return jsonify(data)
